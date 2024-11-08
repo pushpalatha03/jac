0d25cc9d2a4e4fc8046a3dcd9c84b1131f387c02
DarshanAguru <agurudf@gmail.com>
2024-11-08 14:33:28 +0530

    url fix

--- miscellaneousCollection/assets/data/miscCol.js
+++ miscellaneousCollection/assets/data/miscCol.js
@@ -4,14 +4,14 @@ const toolsList = [
         name: "Blog-Json-Builder",
         location: "./blog-json-builder",
         description: "This is a tool to build a json file for blogging purposes. It will take your text input and convert it into a json output.", 
-        href: "./blog-json-builder/blog-json-builder.html"
+        href: "blog-json-builder/blog-json-builder.html"
     },
     {
         id: 2,
         name: "Encoder-Decoder-Generator",
         location: "./encoder-decoder",
         description: "This is a tool to encode and decode text or generate a hash-digest or uuid from given text.",
-        href: "./encoder-decoder/encoder-decoder-generator.html"
+        href: "encoder-decoder/encoder-decoder-generator.html"
 
     }
 ]
