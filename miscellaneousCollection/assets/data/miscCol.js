d576cead96b28ff36f767b40fad7508aced5d3dd
DarshanAguru <agurudf@gmail.com>
2024-11-08 14:19:50 +0530

    first commit

--- miscellaneousCollection/assets/data/miscCol.js
+++ miscellaneousCollection/assets/data/miscCol.js
@@ -4,14 +4,14 @@ const toolsList = [
         name: "Blog-Json-Builder",
         location: "../../blog-json-builder",
         description: "This is a tool to build a json file for blogging purposes. It will take your text input and convert it into a json output.", 
-        href: "/miscellaneousCollection/blog-json-builder/blog-json-builder.html"
+        href: "./miscellaneousCollection/blog-json-builder/blog-json-builder.html"
     },
     {
         id: 2,
         name: "Encoder-Decoder-Generator",
         location: "../../encoder-decoder",
         description: "This is a tool to encode and decode text or generate a hash-digest or uuid from given text.",
-        href: "/miscellaneousCollection/encoder-decoder/encoder-decoder-generator.html"
+        href: "./miscellaneousCollection/encoder-decoder/encoder-decoder-generator.html"
     }
 ]
 
